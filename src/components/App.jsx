85cf43abcef444d281ba6882310407b11563f479
Facundo Zerpa <facuzerpa@bitbucket.org>
2019-06-09 22:15:08 -0300

    Component App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Weather from "./Weather";
 
 function App() {
-  return <h1>Your magic goes here!</h1>;
+  return (
+    <div className="d-flex justify-content-center align-items-center container w-100 m-0 p-0">
+      <div className="row d-flex justify-content-center">
+        <h1 className="text-secondary">Welcome to My Weather</h1>
+        <Weather />
+      </div>
+    </div>
+  );
 }
 
 export default App;
